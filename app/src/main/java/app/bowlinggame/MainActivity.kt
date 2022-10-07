2be7ced2f08b3c5e3c484754cc718b5868de4c4d
CarrotWiz <carrotwiz22@gmail.com>
2022-10-07 12:39:53 -0400

    Home Page w/ signup/login buttons and respective activities

--- app/src/main/java/app/bowlinggame/MainActivity.kt
+++ app/src/main/java/app/bowlinggame/MainActivity.kt
@@ -1,18 +1,31 @@
 package app.bowlinggame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import app.bowlinggame.ui.main.MainFragment
+import android.widget.Button
+import app.bowlinggame.ui.activity.LoginActivity
+import app.bowlinggame.ui.activity.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+        setContentView(R.layout.activity_main)
+        val signUpButton = findViewById<Button>(R.id.signup_button)
+        val loginButton = findViewById<Button>(R.id.login_button)
+        signUpButton.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
+        loginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
     }
 }
---
+++ app/src/main/java/app/bowlinggame/ui/activity/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package app.bowlinggame.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.bowlinggame.R
+
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
